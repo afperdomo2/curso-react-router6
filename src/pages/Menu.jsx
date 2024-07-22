babfcf3d43969fd16b6258e6b86d9f6ddd608d4d
alvarado0925 <alvarado0925@gmail.com>
2024-07-21 23:53:50 -0500

    Implementa NavLink para los botones de navegación del menú

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,5 +1,28 @@
+import { NavLink } from "react-router-dom";
+
 function Menu() {
-  return <nav>Menú</nav>
+  const ROUTES = [
+    { to: "/", label: "Home" },
+    { to: "/blog", label: "Blog" },
+    { to: "/profile", label: "Profile" },
+  ];
+
+  return (
+    <nav>
+      <ul>
+        {ROUTES.map((route, index) => (
+          <li key={index}>
+            <NavLink
+              to={route.to}
+              style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+            >
+              {route.label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export { Menu };
