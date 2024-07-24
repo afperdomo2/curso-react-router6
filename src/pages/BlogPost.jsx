f4380f4f66e774ea4725d09df468be8472a6106d
alvarado0925 <alvarado0925@gmail.com>
2024-07-23 22:15:30 -0500

    Usando el hook de useNavigate para acceder al historial de navegación

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,13 +1,21 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { posts } from "../data/postsData";
 
 function BlogPost() {
+  const navigate = useNavigate();
+
   const { slug } = useParams();
 
   const post = posts.find((post) => post.slug === slug);
 
+  const returnToBlog = () => {
+    navigate(-1); // Se devuelve a la página anterior
+  };
+
   return (
     <>
+      <button onClick={returnToBlog}>⬅️ Back</button>
+
       <Link to="/blog">⬅️ Back</Link>
 
       <h2>{post.title}</h2>
