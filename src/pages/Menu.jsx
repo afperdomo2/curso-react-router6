16a264a50619b4601386932ab344f9206615726e
alvarado0925 <alvarado0925@gmail.com>
2024-07-28 22:39:05 -0500

    Manejando rutas públicas y privadas en el menú

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,27 +1,41 @@
 import { NavLink } from "react-router-dom";
 
+import { useAuth } from "../security/useAuth";
+
 function Menu() {
   const ROUTES = [
-    { to: "/", label: "Home" },
-    { to: "/blog", label: "Blog" },
-    { to: "/profile", label: "Profile" },
-    { to: "/login", label: "Login" },
-    { to: "/logout", label: "Logout" },
+    { to: "/", label: "Home", isPrivate: false },
+    { to: "/blog", label: "Blog", isPrivate: false },
+    { to: "/profile", label: "Profile", isPrivate: true },
+    { to: "/login", label: "Login", isPrivate: false, isPublicOnly: true },
+    { to: "/logout", label: "Logout", isPrivate: true },
   ];
+
+  const auth = useAuth();
 
   return (
     <nav>
       <ul>
-        {ROUTES.map((route, index) => (
-          <li key={index}>
-            <NavLink
-              to={route.to}
-              style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
-            >
-              {route.label}
-            </NavLink>
-          </li>
-        ))}
+        {ROUTES.map((route, index) => {
+          if (route.isPublicOnly && auth.user) {
+            return null;
+          }
+          if (route.isPrivate && !auth.user) {
+            return null;
+          }
+          return (
+            <li key={index}>
+              <NavLink
+                to={route.to}
+                style={({ isActive }) => ({
+                  color: isActive ? "red" : "blue",
+                })}
+              >
+                {route.label}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
